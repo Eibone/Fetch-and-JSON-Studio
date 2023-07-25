b571bd485d0bd28b5b358fa1264f6455851b4ee8
William R <wrowe31@yahoo.com>
2023-07-24 19:59:03 -0500

    Studio finish

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(data) {
+            console.log(data);
+            data.sort(function(a, b){return b.hoursInSpace-a.hoursInSpace});
+            astronautCount = 0;
+            for (let i in data) {
+                container.innerHTML += 
+                `<span class = "astronaut">
+                    <h3>${data[i].firstName} ${data[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${data[i].hoursInSpace}</li>
+                        <li class="${data[i].active ? 'active' : ''}">Active ${data[i].active}</li>
+                        <li>Skills: ${data[i].skills}</li>
+                    </ul>
+                    <img class="avatar" src="${data[i].picture}">
+                </span>
+            `;
+                astronautCount ++;
+            };
+            let counter = document.getElementById("count")
+            counter.textContent = astronautCount;
+        });
+    });
+});
+
